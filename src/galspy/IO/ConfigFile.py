f46376f2c16be517957ec8a8450b8b51cbfcaea3
RanitBehera <ranitbehera1998@gmail.com>
2024-07-03 14:51:12 +0530

    Merged Write from dictionary

--- src/galspy/IO/ConfigFile.py
+++ src/galspy/IO/ConfigFile.py
@@ -1,44 +1,73 @@
+import pathlib,ast
 
-class TextConfig:
-    def __init__(self,path,sep_char='\n',comment_char='#') -> None:
-        self.path   = path
-        self.cmnt   = comment_char
-        self.sep    = sep_char
-    
-    def ReadAsDictionary(self):
-        # Read text file.
-        with open(self.path) as cfg:text = cfg.read()
 
-        # Split into lines.
-        lines = text.split(self.sep)
+def ReadAsDictionary(filepath,sep_char='\n',comment_char='#'):
+    # Check if file exists
+    config_file = pathlib.Path(filepath)
+    if not config_file.exists():
+        raise FileExistsError(f'File "{filepath}" not found')
 
-        # Prepare empty dictionary.
-        config_dict = {}
+    # Read text file.
+    with open(filepath) as cfg:text = cfg.read()
 
-        # For each lines
-        for line in lines:
-            # Remove unwanted space
-            line = line.strip()
+    # Split into lines.
+    lines = text.split(sep_char)
 
-            # Filter out blank lines and full comments.
-            if line=='' or line.startswith(self.cmnt):continue
+    # Prepare empty dictionary.
+    config_dict = {}
 
-            # Filter out the inline comments
-            line = line.split(self.cmnt)[0].strip()
+    # For each lines
+    for line in lines:
+        # Remove unwanted space
+        line = line.strip()
 
-            # Form key-value pair.
-            tokens  = line.split("=")
-            key,value     = tokens[0].strip(),tokens[1].strip()
+        # Filter out blank lines and full comments.
+        if line=='' or line.startswith(comment_char):continue
 
-            # Cast to appropiate types
-            # - String
-            if value.startswith('"') and value.endswith('"'):value = str(value[1:-1])
-            # - Integer : Not implemented
-            # - Float : Not implemented
-            
-            # Fill the dictionary
-            config_dict[key]=value
+        # Filter out the inline comments
+        line = line.split(comment_char)[0].strip()
+
+        # Form key-value pair.
+        tokens  = line.split("=")
+        key,value     = tokens[0].strip(),tokens[1].strip()
+
+        # Cast to appropiate types
+        # - String
+        if value.startswith('"') and value.endswith('"'):value = str(value[1:-1])
+        # - Integer : Not implemented
+        # - Float : Not implemented
         
-        return config_dict
+        value = ast.literal_eval(value)
+
+        # Fill the dictionary
+        config_dict[key]=value
+    
+    return config_dict
+
+
+def WriteFromDictionary(dict:dict,filepath:str,header:str="",header_char="#",overwrite=False):
+
+    # Check if file exists
+    config_file = pathlib.Path(filepath)
+    if config_file.exists() and not overwrite:
+        raise FileExistsError(f'File "{filepath}" already exists and overwrite is restricted')
+
+    with open(filepath,'w') as f:
+        # Header
+        lines = header.split("\n")
+        formatted_header =""
+        for line in lines:
+            if line=='':formatted_header +="\n"
+            else: formatted_header+= '#' + line + '\n'
+        
+        f.write(formatted_header)
+
+        # Dictionary
+        for key,value in dict.items():
+            if isinstance(value,str):value = "\""+value+"\""
+            f.write(f'{key} = {str(value)}\n')
+
+    
+
 
 
