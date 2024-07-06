70e4d38eb97ef77c5c3b4ff070ae94d29d10022b
RanitBehera <ranitbehera1998@gmail.com>
2024-07-07 03:19:29 +0530

    ANSI Support

--- src/galspy/console/terminal.py
+++ src/galspy/console/terminal.py
@@ -6,10 +6,57 @@ import inspect
 
 from prompt_toolkit import prompt, PromptSession
 from prompt_toolkit.completion import WordCompleter, NestedCompleter
-from prompt_toolkit.styles import Style
 from prompt_toolkit.shortcuts import CompleteStyle
 from prompt_toolkit.history import FileHistory
 from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
+from prompt_toolkit.formatted_text import ANSI as PT_ANSI
+
+
+
+# =============================
+# ----- ANSI ESCAPE CODE ------
+# =============================
+class ANSI:
+    def ansi(n):return f"\033[{n}m"
+    RESET           = ansi(0)
+    BOLD            = ansi(1)
+    DIM             = ansi(2)
+    ITALIC          = ansi(3)
+    UNDERLINE       = ansi(4)
+    REVERSE         = ansi(7)
+    STRIKE          = ansi(9)
+    NORMAL          = ansi(22)
+    NO_ITALIC       = ansi(23)
+    NO_UNDERLINE    = ansi(24)
+    NO_REVERSE      = ansi(27)
+    NO_STRIKE       = ansi(29)
+    OVERLINE        = ansi(53)
+    NO_OVERLINE     = ansi(55)
+    FG_BLACK        = ansi(30)
+    FG_RED          = ansi(31)
+    FG_GREEN        = ansi(32)
+    FG_YELLOW       = ansi(33)
+    FG_BLUE         = ansi(34)
+    FG_MAGENTA      = ansi(35)
+    FG_CYAN         = ansi(36)
+    FG_WHITE        = ansi(37)
+    FG_RESET        = ansi(39)
+    BG_BLACK        = ansi(40)
+    BG_RED          = ansi(41)
+    BG_GREEN        = ansi(42)
+    BG_YELLOW       = ansi(43)
+    BG_BLUE         = ansi(44)
+    BG_MAGENTA      = ansi(45)
+    BG_CYAN         = ansi(46)
+    BG_WHITE        = ansi(47)
+    BG_RESET        = ansi(49)
+    def fg_256(n):return f"\033[38;5;{n}m"
+    def bg_256(n):return f"\033[48;5;{n}m"
+    def fg_rgb(r,g,b):return f"\033[38;2;{r};{g};{b}m"
+    def bg_rgb(r,g,b):return f"\033[48;2;{r};{g};{b}m"
+             
+
+
 
 
 # ====================
@@ -27,59 +74,46 @@ class Terminal:
         self.env["HOME"] = os.environ.get("HOME")
         self.env["PATH"] = []
 
-        self.Set_Prompt_Style()
-
+        self.ic = self.Get_Internal_Commands()
 
     def AddPath(self,path:str):
         if path in self.env["PATH"]:return
         self.env["PATH"].append(path)
 
-
-    def Set_Prompt_Style(self):
-        self.pt_style = Style.from_dict(
-            {
-                ""          : "#ffffff",
-                "username"  : "#00ff00 bold",
-                "at"        : "#ffffff bold",
-                "hostname"  : "#ffff00 bold",
-                "colon"     : "#ffffff bold",
-                "workdir"   : "#3fa2f6 bold",
-                "dollar"    : "#ffffff bold"
-            }
-        )
-
+    def Get_Internal_Commands(self):
+        int_cmd = {}
+        for name,obj in inspect.getmembers(sys.modules[__name__]):
+            if not (inspect.isfunction(obj) and obj.__module__ == __name__): continue
+            int_cmd[name]=obj                           
+        return int_cmd
 
     def Get_Prompt(self):    
         prompt_fragmenets = [
-            ("class:username",self.env["USER"]),
-            ("class:at","@"),
-            ("class:hostname",self.env["HOSTNAME"]),
-            ("class:colon"," : "),
-            ("class:workdir",self.env["PWD"].replace(self.env["HOME"],"~")),
-            ("class:dollar","$ ")
+            (ANSI.BOLD+ANSI.fg_256(47),self.env["USER"]),
+            (ANSI.BOLD,"@"),
+            (ANSI.BOLD+ANSI.fg_256(184),self.env["HOSTNAME"]),
+            (ANSI.BOLD," : "),
+            (ANSI.BOLD+ANSI.fg_256(33),self.env["PWD"].replace(self.env["HOME"],"~")),
+            (ANSI.BOLD,"$ ")
         ]
         self.env["PS"]= "".join([val for _,val in prompt_fragmenets])
-        return prompt_fragmenets
+        return "".join(["".join(frag)+ANSI.RESET for frag in prompt_fragmenets])
+        
 
 
     def Get_RPrompt(self):
-        rprompt = ""
+        rprompt = ANSI.BG_CYAN + ANSI.FG_BLACK + " GalSpy " + ANSI.RESET
         return rprompt
 
 
     def Get_Completion(self):
         completion_dict = {}
-        
 
-        internal_commands = [name for name,obj in inspect.getmembers(sys.modules[__name__]) 
-                            if (inspect.isfunction(obj) and
-                                # name.startswith('test') and
-                                obj.__module__ == __name__)]
-
-        print(internal_commands)
-        internal_commands = ("quit","exit","clear","cls","clc","pwd","ls","env","echo","where","which")
+        # Auto-Detect Internal Commands 
+        internal_commands = [key_name for key_name,_ in self.ic.items()]
         for ic in internal_commands:completion_dict[ic]=None
 
+        # Auto-Detect External Commands 
         for path in self.env["PATH"]:
             childs      = [c for c in os.listdir(path)]
             subfiles    = [f for f in childs if os.path.isfile(os.path.join(path, f))]
@@ -104,27 +138,25 @@ class Terminal:
         # Promot Session
         nest_completer  = NestedCompleter.from_nested_dict(self.Get_Completion())
         hist_file       = FileHistory(os.path.join(os.environ.get("HOME"),".gspy_history"))
-        session = PromptSession(style=self.pt_style,
-                                completer=nest_completer,
+        session = PromptSession(completer=nest_completer,
                                 complete_in_thread=True,
                                 history=hist_file,
                                 auto_suggest=AutoSuggestFromHistory()
                                )
-        return
+        
         # Run main loop
         while True:
             # <----- UPDATE PROMPT ----->
-            prompt_fragments = self.Get_Prompt()
+            prompt  = self.Get_Prompt()
             rprompt = self.Get_RPrompt()
 
             # <----- GET COMMAND ----->
-            # command=input(self.env["PS"] + " ")
-            command = session.prompt(prompt_fragments,rprompt=rprompt)
+            # command=input(prompt)
+            command:str = session.prompt(PT_ANSI(prompt),rprompt=PT_ANSI(rprompt))
+                                         
             
             if command=="": continue
-            if command.lower() in ["quit","exit"]:
-                # self._ut.save_history(self.history)
-                break
+            if command.lower() in ["quit","exit"]: break
 
 
             # <----- EXECUTE COMMAND ----->
@@ -158,15 +190,11 @@ class Terminal:
 
                 # Internal Command
                 cmd_exec_lc = cmd_exec.lower()
-                if cmd_exec_lc in ["clear","cls","clc"]: clear()
-                elif cmd_exec_lc in ["pwd"]: pwd(self.env)
-                elif cmd_exec_lc in ["ls"]: ls(self.env,cmd_args)
-                elif cmd_exec_lc in ["env"]: env(self.env,cmd_args)
-                elif cmd_exec_lc in ["echo"]: echo(self.env,cmd_args)
-                elif cmd_exec_lc in ["where","which"]: where(self.env,cmd_args,True)
+                if cmd_exec_lc in self.ic : 
+                    self.ic[cmd_exec_lc](self.env,cmd_args)
                 else:
                     # External Command
-                    exec_path = where(self.env,[cmd_exec])
+                    exec_path = which(self.env,[cmd_exec],False)
                     if len(exec_path)==0:
                         print("Unknown command :",cmd_exec)
                         continue
@@ -193,18 +221,56 @@ class Terminal:
 # ----- INTERNAL COMMANDS -----
 # =============================
 
-def clear():
+
+def clear(env:dict,args:list[str]):
     if os.name=="nt":os.system("cls")
     else: os.system("clear")
 
-cls = clear
 
-def pwd(env:dict):
+def pwd(env:dict,args:list[str]):
     print(env["PWD"])
 
 
 def echo(env:dict,args:list[str]):
     print(" ".join(args))
+
+
+def cd(env:dict,args:list[str]):
+    todir= args[0] if len(args)>0 else env["HOME"]
+    os.chdir(todir)
+    env["PWD"] = os.getcwd()
+
+
+
+def ls(env:dict,args:list[str]):
+    path    = env["PWD"]
+    childs  = [c for c in os.listdir(path)]
+    child_dirs    = []
+    child_files   = []
+    child_links   = []
+    for c in childs:
+        if os.path.isdir(os.path.join(path,c)):
+            child_dirs.append(c)
+        elif os.path.islink(os.path.join(path,c)):
+            child_links.append(c)
+        elif os.path.isfile(os.path.join(path,c)):
+            child_files.append(c)
+
+    
+    # Remove Hidden Childs
+    if not "-a" in args:
+        child_dirs  = [d for d in child_dirs if not d.startswith(".")]
+        child_files = [f for f in child_files if not f.startswith(".")]
+        child_links = [l for l in child_links if not l.startswith(".")]
+
+    # Format Printing
+    END = "\n" if "-l" in args else "  "
+    [print(ANSI.fg_256(33)+d+ANSI.RESET,end=END) for d in child_dirs]
+    [print(ANSI.fg_256(51)+l+ANSI.RESET,os.readlink(os.path.join(path,l)) if "-l" in args else "",sep="  ->  ",end=END) for l in child_links]
+    [print(f,end=END) for f in child_files]
+    
+    print("")
+
 
 
 def env(env:dict,args:list[str]):
@@ -217,7 +283,7 @@ def env(env:dict,args:list[str]):
         print(str(key).rjust(max_len),"=",env[key])
 
 
-def where(env:dict,args:list[str],print_list:bool=False):
+def which(env:dict,args:list[str],print_list:bool=True):
     found_path=[]
     for path in env["PATH"]:
         childs      = [c for c in os.listdir(path)]
@@ -230,16 +296,6 @@ def where(env:dict,args:list[str],print_list:bool=False):
         for path in found_path:print(path)
     return found_path 
 
-
-def ls(env:dict,args:list[str]):
-    path = env["PWD"]
-    childs = [c for c in os.listdir(path)]
-    for c in childs:
-        if os.path.isdir(os.path.join(path,c)):
-            print("",c,end="\t")
-        elif os.path.isfile(os.path.join(path,c)):
-            print(c,end="\t")
-    print("")
 
 
 
