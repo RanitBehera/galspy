695cd9d3808731e9e4ae104ac4d8201e7a97ad56
RanitBehera <ranitbehera1998@gmail.com>
2024-12-24 10:54:48 +0530

    Save Progress

--- scripts/get_centers/centers_m3.py
+++ scripts/get_centers/centers_m3.py
@@ -9,7 +9,7 @@ from scipy.ndimage import gaussian_filter
 from matplotlib.gridspec import GridSpec
 
 
-def GetHitogramProfile(data,bins=1000,apply_log10=True):
+def GetHitogramProfilePeaks(data,bins=1000,apply_log10=True):
     hist, bin_edges = np.histogram(data,bins=bins)
     bin_centers = (bin_edges[:-1] + bin_edges[1:]) / 2
     if apply_log10:
@@ -20,23 +20,24 @@ def GetHitogramProfile(data,bins=1000,apply_log10=True):
     return bin_centers,sm_log_hist,peaks
 
 
-def FindProjectionPeaks(positions):
+def FindProjectionPeaks(positions,mass,uselog=True,reclevel=0):
+    print("Recursion Level",reclevel)
     x,y,z=positions.T
     tree = KDTree(positions)
 
 
-    # ========================
-    # 1. CANDIDATE PEAKS
-    # ========================
+    # =========================
+    # 1. FIND PEAKS CANDIDATES
+    # =========================
     # Find peaks in 1D histograms of x,y,z
 
-    binx,sm_logcountx,peaksx = GetHitogramProfile(x)
-    biny,sm_logcounty,peaksy = GetHitogramProfile(y)
-    binz,sm_logcountz,peaksz = GetHitogramProfile(z)
+    binx,sm_logcountx,peaksx = GetHitogramProfilePeaks(x,apply_log10=uselog)
+    biny,sm_logcounty,peaksy = GetHitogramProfilePeaks(y,apply_log10=uselog)
+    binz,sm_logcountz,peaksz = GetHitogramProfilePeaks(z,apply_log10=uselog)
 
     candidates = list(itertools.product(peaksx,peaksy,peaksz))
     
-    if True:
+    if False:
         fig = plt.figure(figsize=(12,8))
         gs = GridSpec(3,2,figure=fig)
         ax_px = fig.add_subplot(gs[0,0])
@@ -54,8 +55,8 @@ def FindProjectionPeaks(positions):
                 ax.axvline(p,color='k',lw=1)
 
         cv = CubeVisualizer(ax_3d)
-        cv.add_points(positions,points_size=1,points_color='k')
-        cv.add_points(candidates,points_size=50,points_color='r',points_marker='+')
+        cv.add_points(positions,points_size=1,points_color='r')
+        cv.add_points(candidates,points_size=50,points_color='b',points_marker='+')
         cv.show(False)
     
     
@@ -114,17 +115,85 @@ def FindProjectionPeaks(positions):
     # ============================
     # 4. BOUNDARY FINDING
     # ============================
-    cntr_bnd = []
-    
+    _PLOT=False
+    if _PLOT:
+        fig,axs= plt.subplots(len(opt_centers),1)
+        if len(opt_centers)==1:axs=[axs]
+        axs:list[plt.Axes]
+
+    cntr_rad = []
+    for i,cntr in enumerate(opt_centers):
+        dist,idx = tree.query(cntr,len(positions))
+        count,bin_edges = np.histogram(dist,bins=1000)
+        bin_centers = (bin_edges[:-1] + bin_edges[1:]) / 2
+        
+        logcount = np.log10(1+count)
+        dist_logcount = gaussian_filter(logcount,sigma=5)
+        if _PLOT: axs[i].plot(bin_centers,dist_logcount)
+
+        slope_sign = np.sign(np.gradient(dist_logcount))
+        if _PLOT: axs[i].plot(bin_centers,slope_sign,alpha=0.2)
+
+        triggers = np.diff(slope_sign)
+        if _PLOT: axs[i].plot(bin_centers[:-1],triggers,alpha=0.2)
+
+        boundary = bin_centers[np.where(triggers>0)][0]
+        if _PLOT: axs[i].axvline(boundary)
+
+        cntr_rad.append([cntr,boundary])
+
+
+    # ===============================
+    # 4. COLLIDING BOUNDARY SHRINK
+    # ===============================
+    for a,(anchor,arad) in enumerate(cntr_rad):
+        for t,(target,trad) in enumerate(cntr_rad):
+            if a>=t:continue
+            ctr_dist = np.linalg.norm(target-anchor)
+            rad_sum   = arad + trad
+            if rad_sum<ctr_dist:continue
+            
+            ratio =rad_sum/ctr_dist
+            cntr_rad[a]=[anchor,arad/ratio]
+            cntr_rad[t]=[target,trad/ratio]
+
+
+    # =================================
+    # 5. MASS CALCULATION
+    # =================================
+    sub_mass = []
+    accounted_ind = []
+    for i,(anchor,arad) in enumerate(cntr_rad):
+        bound_part_ind = tree.query_ball_point(anchor,arad)
+        bound_part_mass = np.sum(mass[bound_part_ind])
+        sub_mass.append(bound_part_mass)
+        accounted_ind.extend(bound_part_ind)
 
 
 
-    plt.show()
 
 
 
+    table=[]
+    for (cntr,rad),bmass in zip(cntr_rad,sub_mass):
+        table.append([*cntr,rad,bmass])
+    table=np.array(table)
 
 
+    # =================================
+    # 6. RECURSION
+    # =================================
+
+    mask = np.zeros(len(positions),dtype=bool)
+    mask[accounted_ind]=True
+    upos = positions[~mask]
+    umass = mass[~mask]
+
+    if reclevel<2:
+        table2=FindProjectionPeaks(upos,umass,True,reclevel+1)
+        table = np.vstack([table,table2])
+
+    return table    
 
 
 
@@ -218,17 +287,30 @@ print("[ ANALYSING GROUPS ]")
 lencids = len(cids)
 
 
-
 for i,cid in enumerate(cids):
-    if not cid==1:continue
+    if not cid==5:continue
     # if cid>20:break
 
     # if cid not in okay:continue
 
     print(f"- GroupID : {cid} ({i+1}/{lencids})")
-    # try:
-    FindProjectionPeaks(spos[sgid==cid])
-    # except:
-    #     # pass
-    #     fp.write(f"#{cid}\n")
-    #     continue
+    cpos = spos[sgid==cid]
+    cmass = smass[sgid==cid]
+    table=FindProjectionPeaks(cpos,cmass)
+
+
+    if True:
+        fig = plt.figure()
+        cv = CubeVisualizer()
+        cv.add_points(cpos,points_alpha=0.5,points_color='r')
+
+        for cx,cy,cz,r,m in table:
+            cntr=np.array([cx,cy,cz])
+            cv.add_points([cntr],points_color='k',points_size=1000,points_marker='+')
+            cv.add_sphere_wire(cntr,r,'b')
+
+
+        ax=cv.show(False)
+        ax.set_title(f"GID={cid} : N={len(table)}")
+
+        plt.show()
