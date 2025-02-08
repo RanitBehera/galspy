2311b613fa0be2a3a7ce5b309da10401b6475a53
RanitBehera <ranitbehera1998@gmail.com>
2025-02-08 06:50:16 +0530

    Save Progress OpenCV

--- scripts/SPM2/light_v2.py
+++ scripts/SPM2/light_v2.py
@@ -5,6 +5,7 @@ from scipy.signal import convolve2d
 from typing import Literal
 import matplotlib.pyplot as plt
 import cv2 as cv
+from typing import List
 
 from galspy.utility.visualization import CubeVisualizer
 
@@ -121,47 +122,25 @@ class ClumpManager:
 
 
             # Get the bounding box of the ellipse
-            # print(axes)
-            min_x = center[0] - axes[0]
-            max_x = center[0] + axes[0]
-            min_y = center[1] - axes[1]
-            max_y = center[1] + axes[1]
+            max_axis=max(axes)
 
-            # for x in range(center[0]-5,center[0]+6):
-            #     label_img[center[1],x]=1
-            # for y in range(center[1]-5,center[1]+6):
-            #     label_img[y,center[0]]=1
-
+            min_x = center[0] - max_axis - 4
+            max_x = center[0] + max_axis + 4
+            min_y = center[1] - max_axis - 4
+            max_y = center[1] + max_axis + 4
             
-
-            # Loop over the bounding box and check if the pixel is inside the ellipse
+            # Loop over the bounding box and check if the pixel is inside the box
             for y in range(min_y, max_y):
                 for x in range(min_x, max_x):
-                    label_img[y,x]=1
-                # break
-                #     # Ensure that the pixel is within image bounds
-                #     if 0 <= x < img.shape[1] and 0 <= y < img.shape[0]:
-                #         # Translate pixel coordinates relative to ellipse center
-                #         dx = y - center[1]
-                #         dy = x - center[0]
-                        
-                #         # Ellipse equation (standard form)
-                #         if (dx**2 / axes[0]**2 + dy**2 / axes[1]**2) <= 1:
-                #             # Assign this pixel to the current ellipse
-                #             label_img[y, x] = ellipse_index
-                #         else:
-                #             label_img[y,x]=-2
+                    label_img[y,x]=ellipse_index
 
             ellipse_index += 1
-
-
-
 
             # Draw Overlays
             cv.ellipse(overlay_img, center,axes,-angle, 0,365,(0, 0, 255), 1)
             cv.line(overlay_img, center, (major_axis_end), (0, 255, 0), 1)
             cv.line(overlay_img, center, (minor_axis_end), (255, 0, 0), 1)
-
+            cv.rectangle(overlay_img,(min_x,min_y),(max_x,max_y),(255,255,255),1)
 
 
 
@@ -183,6 +162,69 @@ class ClumpManager:
         }
     
 
+    def ShowOpenCVPipeline(self,cvout=None,mode:Literal["all","major"]="major"):
+        if cvout==None:
+            mg = cm.GetProjection()
+            cvout = cm.OpenCVPostProcess(img)
+        
+        if mode=="all":
+            fig,axs = plt.subplots(2,4,figsize=(9,6),sharex=True, sharey=True)
+            axs:np.ndarray
+            axs=axs.flatten()
+        elif mode=="major":
+            fig,axs = plt.subplots(1,3,figsize=(9,6),sharex=True, sharey=True)
+
+        iter_axs=iter(axs)
+        def next_ax():
+            return next(iter_axs)
+        
+        if mode in ["all"]:
+            ax:plt.Axes=next_ax()    
+            ax.imshow(cvout["INPUT_IMG"].T,origin='lower',cmap="grey")
+            ax.set_title("Projection Original")
+
+        if mode in ["all","major"]:
+            ax=next_ax()    
+            ax.imshow(cvout["BINARY_IMG"].T,origin='lower',cmap="grey")
+            ax.set_title(f"Binary")
+
+        if mode in ["all","major"]:
+            ax=next_ax()    
+            ax.imshow(cvout["BINARY_TH_IMG"].T,origin='lower',cmap="grey")
+            ax.set_title(f"Binary Thresold : S={cvout['TH_MIN_STAR_COUNT']}")
+
+        if mode in ["all"]:
+            ax=next_ax()    
+            ax.imshow(cvout["MORPH_CLOSED"].T,origin='lower',cmap="grey")
+            ax.set_title(f"Morphological (Closed)")
+        
+        if mode in ["all"]:
+            ax=next_ax()    
+            ax.imshow(cvout["MORPH_OPENED"].T,origin='lower',cmap="grey")
+            ax.set_title(f"Morphological (Opened)")
+
+        if mode in ["all"]:
+            ax=next_ax()    
+            ax.imshow(cvout["MORPH_DILATED"].T,origin='lower',cmap="grey")
+            ax.set_title(f"Morphological (Dilation)")
+
+        if mode in ["all","major"]:
+            ax=next_ax()    
+            ax.imshow(np.transpose(cvout["OVERLAY_IMG"],(1,0,2)),origin='lower')
+            ax.set_title(f"Overlay : N={cvout['BLOB_COUNT']}")
+
+        if mode in ["all"]:
+            ax=next_ax()    
+            ax.imshow(cvout["LABEL_IMG"].T,origin='lower')
+            ax.set_title(f"Label Map")
+
+        for ax in axs:
+            ax.set_axis_off()
+
+
+        # plt.tight_layout()
+        plt.show()
+
 
 
 SNAPSPATH = "/mnt/home/student/cranit/NINJA/simulations/L150N2040/SNAPS/"
@@ -193,54 +235,20 @@ stellar_mass = galspy.NavigationRoot(SNAPSPATH).PIG(SNAPNUM).FOFGroups.MassByTyp
 
 ##%%
 for i in range(1,100):
-    if i not in [1]:continue
+    if i not in [1,7]:continue
     print(i)
     st_mass = stellar_mass[i-1]
     print("  ","Stellar Mass :",st_mass,"e10")
 
 
     cm = ClumpManager(SNAPSPATH,SNAPNUM,i)
-    img=cm.GetProjection()
+    img = cm.GetProjection()
     cvout = cm.OpenCVPostProcess(img)
+    cm.ShowOpenCVPipeline(cvout)
     # cm.ShowCube()
 
 
 
     # PLOT
-    fig,axs = plt.subplots(3,4,figsize=(9,6))
-    ax1,ax2,ax3,ax4 = axs[0,0],axs[0,1],axs[0,2],axs[0,3]
-    ax5,ax6,ax7,ax8 = axs[1,0],axs[1,1],axs[1,2],axs[1,3]
-    ax9,ax10,ax11,ax12 = axs[2,0],axs[2,1],axs[2,2],axs[2,3]
-
-    ax1.imshow(cvout["INPUT_IMG"].T,origin='lower',cmap="grey")
-    ax1.set_title("Projection Original")
-
-    ax2.imshow(cvout["BINARY_IMG"].T,origin='lower',cmap="grey")
-    ax2.set_title(f"Binary")
-
-    ax3.imshow(cvout["BINARY_TH_IMG"].T,origin='lower',cmap="grey")
-    ax3.set_title(f"Binary Thresold : S={cvout['TH_MIN_STAR_COUNT']}")
-
-    ax4.imshow(cvout["MORPH_CLOSED"].T,origin='lower',cmap="grey")
-    ax4.set_title(f"Morphological (Closed)")
     
-    ax5.imshow(cvout["MORPH_OPENED"].T,origin='lower',cmap="grey")
-    ax5.set_title(f"Morphological (Opened)")
-
-    ax6.imshow(cvout["MORPH_DILATED"].T,origin='lower',cmap="grey")
-    ax6.set_title(f"Morphological (Dilation)")
-
-    ax7.imshow(np.transpose(cvout["OVERLAY_IMG"],(1,0,2)),origin='lower')
-    ax7.set_title(f"Overlay : N={cvout['BLOB_COUNT']}")
-
-    ax8.imshow(cvout["LABEL_IMG"].T,origin='lower')
-    ax8.set_title(f"Label Map")
-
-    for ax in [globals()[f"ax{i}"] for i in range(1,13)]:
-        ax:plt.Axes
-        ax.set_axis_off()
-
-
-    # plt.tight_layout()
-    plt.show()
 
