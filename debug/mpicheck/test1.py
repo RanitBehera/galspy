6a64402797ab8a0ec1ed8070963ea1dec29f1bf6
RanitBehera <ranitbehera1998@gmail.com>
2025-04-05 07:23:12 +0530

    Save Changes

---
+++ debug/mpicheck/test1.py
@@ -0,0 +1,7 @@
+from mpi4py import MPI
+import os
+
+comm = MPI.COMM_WORLD
+
+rank = comm.Get_rank()
+print(f"{os.environ['HOSTNAME']} > Hi {os.environ['USER']}. I got rank {rank}.")
--- submit_pbs.sh
+++ submit_pbs.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
-#PBS -N "MAB8"
-#PBS -q "regular"
-#PBS -l walltime=24:00:00
+#PBS -N "CW"
+#PBS -q "debug"
+#PBS -l walltime=1:00:00
 #PBS -l select=1:ncpus=4
 #PBS -o "/dev/null"
 #PBS -e "/dev/null"
 #PBS -j oe
-#PBS -M "ranit.behera@iucaa.in"
-#PBS -m abe
+##PBS -M "ranit.behera@iucaa.in"
+##PBS -m abe
 
 
 # ===== JOB =====
 cd $PBS_O_WORKDIR
-# SUFFIX=primordial
 SUFFIX=solar
-BOX=L150N2040
-Z=10
-exec > $PBS_O_WORKDIR/stdout_pbs/stdout_$SUFFIX_$BOX_$Z.txt 2>&1
+exec > $PBS_O_WORKDIR/log/stdout_$SUFFIX.txt 2>&1
 
 source $HOME/anaconda3/etc/profile.d/conda.sh
 echo "[ STARTED ] $(date)"
